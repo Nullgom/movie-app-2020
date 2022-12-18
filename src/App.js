aae1fc0fc24a6bda5c0a73a858402e174808f7a9
Nullgom <surfwon@gmail.com>
2022-12-18 17:54:07 +0900

    04-02 map() 함수로 컴포넌트 많이 만들기

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React from 'react';
 
-function Food({ fav }) {
-  return <h1>I like {fav}</h1>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: 'Kimchi',
+    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
+  },
+  {
+    name: 'Samgyeopsal',
+    image: 'https://static.hubzum.zumst.com/hubzum/2018/02%2F05%2F10%2F00ada3e050e9403ba3214006a9e2b5fe.jpg',
+  },
+  {
+    name: 'Bibimbap',
+    image: 'https://health.chosun.com/site/data/img_dir/2021/01/27/2021012702508_0.jpg',
+  },
+  {
+    name: 'Doncasu',
+    image: 'http://th2.tmon.kr/thumbs/image/423/336/e17/1b1492318_700x700_95_FIT.jpg',
+  },
+  {
+    name: 'Kimbap',
+    image: 'https://t1.daumcdn.net/news/202211/21/moneyweek/20221121182836765ezcf.jpg',
+  }
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukumi" />
+      {foodILike.map(dish => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
