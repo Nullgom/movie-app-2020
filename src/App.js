8b422724090a401105b6e8d77491824331c724bd
Nullgom <surfwon@gmail.com>
2022-12-18 19:25:55 +0900

    05-2 숫자 증감 기능을 제대로 만들어 보기

--- src/App.js
+++ src/App.js
@@ -1,64 +1,33 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
+// React.Component 클래스의 기능을 추가한 App 클래스
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: 'Kimchi',
-    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: 'Samgyeopsal',
-    image: 'https://static.hubzum.zumst.com/hubzum/2018/02%2F05%2F10%2F00ada3e050e9403ba3214006a9e2b5fe.jpg',
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: 'Bibimbap',
-    image: 'https://health.chosun.com/site/data/img_dir/2021/01/27/2021012702508_0.jpg',
-    rating: 4.5,
-  },
-  {
-    id: 4,
-    name: 'Doncasu',
-    image: 'http://th2.tmon.kr/thumbs/image/423/336/e17/1b1492318_700x700_95_FIT.jpg',
-    rating: 3.8,
-  },
-  {
-    id: 5,
-    name: 'Kimbap',
-    image: 'https://t1.daumcdn.net/news/202211/21/moneyweek/20221121182836765ezcf.jpg',
-    rating: 4,
+  add = () => {
+    this.setState(current => ({
+      count: current.count + 1
+    }));
   }
-];
 
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-      ))}
-    </div>
-  );
+  minus = () => {
+    this.setState(current => ({
+      count: current.count - 1
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+
+  }
 }
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
 
 export default App;
