9a3a6cb72b50e7bb119161684d27e1a360ec9e63
Nullgom <surfwon@gmail.com>
2022-12-18 21:31:58 +0900

    05-4 영화 앱 만들기 워밍업

--- src/App.js
+++ src/App.js
@@ -2,49 +2,20 @@ import React from 'react';
 
 // React.Component 클래스의 기능을 추가한 App 클래스
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('hello');
-  }
-
   state = {
-    count: 0,
+    isLoading: true,
   };
 
-  add = () => {
-    this.setState(current => ({
-      count: current.count + 1
-    }));
-  }
-
-  minus = () => {
-    this.setState(current => ({
-      count: current.count - 1
-    }));
-  }
-
   componentDidMount() {
-    console.log('component rendered');
-  }
-
-  componentDidUpdate() {
-    console.log('I just updated');
-  }
-
-  componentWillUnmount() {
-    console.log('Goodbye, cruel world');
+    // 영화 데이터 로딩!
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000)
   }
 
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
-
+    const { isLoading } = this.state;
+    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
   }
 }
 
